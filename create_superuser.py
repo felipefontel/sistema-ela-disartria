02e94d820cf9d71176df47bc8d52711487a2755e
Felipe Fontenele <felipefontel@gmail.com>
2026-03-04 15:26:23 -0300

    fix: revert soft delete and column is_active from patient due to azure postgresql permission issues

--- create_superuser.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import get_user_model
-import sys
-
-User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'admin')
-    print("Superuser 'admin' created with password 'admin'.")
-else:
-    print("Superuser already exists.")
